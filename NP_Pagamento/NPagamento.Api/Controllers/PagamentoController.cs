49a2f5c63a2dfe162bd8daa1071c0983c71067ad
Antonio Lucas <lucas.d1995@gmail.com>
2021-01-05 19:36:56 -0300

    [Backend] - Criação da controladora e Definição da rota de consumo.

---
+++ NP_Pagamento/NPagamento.Api/Controllers/PagamentoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NPagamento.Core.Responses;
+using NPagamento.Core.Models;
+
+namespace NPagamento.Api.Controllers
+{
+    [ApiController]
+    public class PagamentoController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/pagamentos/compras")]
+        public ActionResult<CompraResponse> AutorizaPagamento(Compra compra) 
+            => new CompraResponse() 
+            {   Valor = compra.valor, 
+                Estado = compra.valor > 100 ? "APROVADO" : "REJEITADO" 
+            };
+    }
+}
---
+++ NP_Pagamento/NPagamento.Api/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NPagamento.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment CurrentEnvironment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            CurrentEnvironment = env;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseCors("SiteCorsPolicy");
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
