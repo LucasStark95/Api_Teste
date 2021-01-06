7a3eda34bea89182405457333d7645acbabd353c
Antonio Lucas <lucas.d1995@gmail.com>
2021-01-05 21:12:51 -0300

    [Backend] - Migração de correção para o nome da entidade.

---
+++ NP_TESTE/NPista.Data/Migrations/20210106000948_AleteracaoNomeEntidade.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NPista.Data.Migrations
+{
+    public partial class AleteracaoNomeEntidade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compras");
+
+            migrationBuilder.CreateTable(
+                name: "Vendas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CartaoId = table.Column<int>(type: "integer", nullable: false),
+                    ProdutoId = table.Column<int>(type: "integer", nullable: false),
+                    QtdeComprada = table.Column<int>(type: "integer", nullable: false),
+                    DataCompra = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValue: new DateTime(2021, 1, 5, 21, 9, 48, 499, DateTimeKind.Local).AddTicks(8536))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Cartoes_CartaoId",
+                        column: x => x.CartaoId,
+                        principalTable: "Cartoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vendas_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_CartaoId",
+                table: "Vendas",
+                column: "CartaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vendas_ProdutoId",
+                table: "Vendas",
+                column: "ProdutoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vendas");
+
+            migrationBuilder.CreateTable(
+                name: "Compras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    CartaoId = table.Column<int>(type: "integer", nullable: false),
+                    DataCompra = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValue: new DateTime(2021, 1, 5, 17, 28, 2, 870, DateTimeKind.Local).AddTicks(2572)),
+                    ProdutoId = table.Column<int>(type: "integer", nullable: false),
+                    QtdeComprada = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Compras_Cartoes_CartaoId",
+                        column: x => x.CartaoId,
+                        principalTable: "Cartoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Compras_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_CartaoId",
+                table: "Compras",
+                column: "CartaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_ProdutoId",
+                table: "Compras",
+                column: "ProdutoId");
+        }
+    }
+}
